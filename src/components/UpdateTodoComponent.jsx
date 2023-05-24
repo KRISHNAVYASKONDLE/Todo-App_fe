f458796c2d6315e793d4a398455bbe61aadcf0ca
KRISHNAVYASKONDLE <krishnavyaskondle@gmail.com>
2023-05-24 08:52:21 +0530

    formik validation

--- src/components/UpdateTodoComponent.jsx
+++ src/components/UpdateTodoComponent.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useAuth } from "./todo/security/AuthContext"
 import { updatetodoapiservicelink } from "./todo/TodoApiService"
 import { useParams } from "react-router-dom"
-import { Formik, Field, Form } from "formik"
+import { Formik, Field, Form, ErrorMessage } from "formik"
 
 export default function UpdateTodoComponent() {
 
@@ -22,18 +22,38 @@ export default function UpdateTodoComponent() {
         }).catch(error => console.log(error))
     }
 
-
     function onSubmit(values) {
         console.log(values)
     }
 
+    function validate(values) {
+        let errors = {}
+
+        if (values.description.length < 5) {
+            errors.description = 'minimum length is 5 for description'
+        }
+        if (values.targetDate == null || values.targetDate <= new Date()) {
+            errors.description = 'date field cant be empty'
+        }
+        return errors
+    }
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                <Formik initialValues={{ description, targetDate }} enableReinitialize={true} onSubmit={onSubmit}>
+                <Formik initialValues={{ description, targetDate }} enableReinitialize={true} onSubmit={onSubmit} validate={validate}
+                    validateOnChange={false}
+                    validateOnBlur={false}
+
+                >
                     {(props) => (
                         <Form>
+                            <ErrorMessage name="description" component="div" className="alert alert-warning" />
+                            <ErrorMessage
+                                name="targetDate"
+                                component="div"
+                                className="alert alert-warning"
+                            />
                             <fieldset>
                                 <label>Description</label>
                                 <Field className="form-control" type="text" name="description" />
